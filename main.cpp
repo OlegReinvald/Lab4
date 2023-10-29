32bbe05cfff0a19cf105fe432ecf803e40623859
Oleg <olejatbwp@gmail.com>
2023-10-29 23:35:09 +0300

    Fixed bug

--- main.cpp
+++ main.cpp
@@ -17,50 +17,59 @@ const char* my_strpbrk(const char* str1, const char* str2) { // Function strpbrk
 }
 
 
-    int main() {
-        char text[300];
-        cout << "Enter string: " << "\n";
-        cin.getline(text, 300, '\n');
-        bool is_digit = true;
-        int max_ones = 0;
-        char temp_word[300];
-        char word_max_ones[300];
-        int temp_ones = 0;
-        int num = 1;
-        int pre_fin_num = -1;
-        int fin_num = -1;
-        for(int i = 0; text[i] != '\0'; i++){
-            if (text[i] == ' '){
-                num ++;
-                if (temp_ones >= max_ones and temp_ones != 0 and is_digit){
-                    max_ones = temp_ones;
-                    pre_fin_num = fin_num;
-                    fin_num = num - 1;
-                }
-                is_digit = true;
-                temp_ones = 0;
-                continue;
+int main() {
+    char text[300];
+    cout << "Enter string: " << "\n";
+    cin.getline(text, 300, '\n');
+    bool is_digit = true;
+    int max_ones = 0;
+    char temp_word[300];
+    char word_max_ones[300];
+    int temp_ones = 0;
+    int num = 1;
+    int pre_fin_num = -1;
+    int fin_num = -1;
+    for(int i = 0; text[i] != '\0'; i++){
+        if (text[i] == ' '){
+            num ++;
+            if (temp_ones > max_ones and temp_ones != 0 and is_digit){
+                max_ones = temp_ones;
+                fin_num = num - 1;
             }
+            if (temp_ones == max_ones and temp_ones != 0 and is_digit){
+                max_ones = temp_ones;
+                pre_fin_num = fin_num;
+                fin_num = num - 1;
+            }
+            is_digit = true;
+            temp_ones = 0;
+            continue;
+        }
 
-            if (int('0') >= int(text[i]) or int('9') <= int(text[i]) and text[i] != ' '){
-                is_digit = false;
-            }
-            if (text[i] == '1'){
-                    temp_ones ++;
-            }
+
+        if (int('0') >= int(text[i]) or int('9') <= int(text[i]) and text[i] != ' '){
+            is_digit = false;
         }
-        num++;
-        if (temp_ones >= max_ones and is_digit){
-            max_ones = temp_ones;
-            pre_fin_num = fin_num;
-            fin_num = num- 1;
-        }
-        if (pre_fin_num == -1){
-            pre_fin_num = fin_num;
-        }
-        if (fin_num == -1){
-            cout << "No numbers with ones";
-        } else {
-            cout << "Word #" << pre_fin_num;
+        if (text[i] == '1'){
+            temp_ones ++;
         }
     }
+    num++;
+    if (temp_ones > max_ones and temp_ones != 0 and is_digit){
+        max_ones = temp_ones;
+        fin_num = num - 1;
+    }
+    if (temp_ones == max_ones and temp_ones != 0 and is_digit){
+        max_ones = temp_ones;
+        pre_fin_num = fin_num;
+        fin_num = num - 1;
+    }
+    if (pre_fin_num == -1){
+        pre_fin_num = fin_num;
+    }
+    if (fin_num == -1){
+        cout << "No numbers with ones";
+    } else {
+        cout << "Word #" << pre_fin_num;
+    }
+}
